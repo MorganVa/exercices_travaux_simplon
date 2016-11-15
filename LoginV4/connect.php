2c8236bcb95cd60e36d118237d73ecff49da07f7
simpmon-solennB <solenn.baer@gmail.com>
2016-11-15 10:48:23 +0100

    suppression de fichiers

--- LoginV4/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "Root#1234";
-$database_name = "loginv4;charset=utf8";
-$pdo = new PDO("mysql:host=$host;dbname=$database_name", $user, $password, array(
-PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-));
-?>
